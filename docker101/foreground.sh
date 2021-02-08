edeb718ce3e9b47b9d140ab38e3afa337ae786f5
SaranOnuan <saranonuan@gmail.com>
2021-02-08 10:22:56 +0700

    Try vscode config

--- docker101/foreground.sh
+++ docker101/foreground.sh
@@ -1,6 +1,7 @@
 echo "Welcome!"
-mkdir -p /root/training
-cd /root/training
+mkdir -p /root/labdocker101
+cd /root/labdocker101
 git clone https://github.com/karthequian/docker-helloworld.git
+cd docker-helloworld
 pwd
 ls -lha
