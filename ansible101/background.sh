11a28b35c75b5171a279db16f75fd4e763da26aa
SaranOnuan <saranonuan@gmail.com>
2021-03-07 16:52:55 +0700

    Add ansiblelab101

--- ansible101/background.sh
+++ ansible101/background.sh
@@ -1,4 +1,1 @@
 #!/bin/bash
-apt-add-repository ppa:ansible/ansible
-apt update
-apt install ansible
--- ansible101/foreground.sh
+++ ansible101/foreground.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Welcome to Ansible 101!"
+echo "Preparing Ansible 101 lab"
+mkdir -p /root/labansible101
+cd /root/labansible101
+git clone https://github.com/saranonuan/kmitl-sdte-ansible-lab.git .
+rm -rf .git
+clear
+echo "Welcome to Ansible 101 Lab!"
+pwd
+ls -la
+ansible --version
