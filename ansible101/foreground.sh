b4cb90fb75fc3432bb679cdc09d71d034bd29866
SaranOnuan <saranonuan@gmail.com>
2021-03-07 16:57:12 +0700

    Add more setup lab

--- ansible101/foreground.sh
+++ ansible101/foreground.sh
@@ -5,7 +5,9 @@ cd /root/labansible101
 git clone https://github.com/saranonuan/kmitl-sdte-ansible-lab.git .
 rm -rf .git
 clear
+mkdir -p /root/labansible101/lab
 echo "Welcome to Ansible 101 Lab!"
 pwd
 ls -la
 ansible --version
+ansible-playbook --version
